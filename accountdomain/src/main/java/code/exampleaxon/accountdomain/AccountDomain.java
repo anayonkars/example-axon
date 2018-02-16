447d90c581ac184da960bfe31004e2e4c862bbb1
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2018-02-16 23:05:32 +0530

    Spring Boot for Account Domain

---
+++ accountdomain/src/main/java/code/exampleaxon/accountdomain/AccountDomain.java
@@ -0,0 +1,11 @@
+package code.exampleaxon.accountdomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountDomain {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountDomain.class, args);
+    }
+}
